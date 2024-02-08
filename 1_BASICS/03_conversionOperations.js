7a5b816d7ac12ee8e6204bd880c24cea05e017b0
Nikhil Kumar <nikhilkumar.pr6@gmail.com>
2024-02-08 19:03:23 +0530

    conversion

---
+++ 1_BASICS/03_conversionOperations.js
@@ -0,0 +1,17 @@
+let score = "40"
+console.log(typeof score)
+
+let value = Number(score) // conversion of string to number 
+console.log(typeof value)
+
+let a = "263absj"
+let b = Number(a)
+console.log(b)    // imp -> it will give "NaN" i.e not a number
+
+// "33" = 33
+//"33abc" = NaN
+// true = 1
+// 1 to boolean = true
+//"" = false
+//"nkhil" = true
+
