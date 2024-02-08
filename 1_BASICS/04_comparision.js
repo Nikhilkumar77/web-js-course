6960d82898726aaf5b06b67fc7c41bce008a447a
Nikhil Kumar <nikhilkumar.pr6@gmail.com>
2024-02-08 21:20:47 +0530

    comparision

---
+++ 1_BASICS/04_comparision.js
@@ -0,0 +1,7 @@
+console.log("2" > 1);
+console.log("02" > 1);
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);// euality check(==) and comparisions works differently on js as in js comparisions treat null as 0
+//=== strict chek check value + datatype
+console.log("2" === 2); // false
