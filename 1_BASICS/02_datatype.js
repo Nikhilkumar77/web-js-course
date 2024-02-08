65ce8075ed8134894b7dec14f67de527146a0518
Nikhil Kumar <nikhilkumar.pr6@gmail.com>
2024-02-08 18:51:00 +0530

    datatypes

---
+++ 1_BASICS/02_datatype.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js codse as newer version
+
+//alert(3 + 3) we are using nodejs not browser
+
+
+// number = 2 to power 53
+//bigint
+// string = ""
+//boolean = true/false
+// null = standalone value
+//undefined = empty value
+//symbol = unique 
+
+//object
+
+console.log(typeof "nikhil");
+console.log(typeof null);  // imp ->   null is of object type 
